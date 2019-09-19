eb452c70ae8eda69d5c3454afd807a451b222fe3
Coldairarrow <xgc15657519397@outlook.com>
2019-09-19 22:37:12 +0800

    JWT

---
+++ src/Coldairarrow.Api/00Filter/JWT校验/CheckJWTAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Coldairarrow.Util;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+
+namespace Coldairarrow.Api
+{
+    /// <summary>
+    /// JWT校验
+    /// </summary>
+    public class CheckJWTAttribute : Attribute, IActionFilter
+    {
+        /// <summary>
+        /// Action执行之前执行
+        /// </summary>
+        /// <param name="context">过滤器上下文</param>
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            string token = context.HttpContext.Request.Headers["Authorization"].ToString();
+
+            string tmp = string.Empty;
+        }
+
+        /// <summary>
+        /// Action执行完毕之后执行
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
