eb653c06107e50d648791de9d5d1c50ab9e2c391
Coldairarrow <xgc15657519397@outlook.com>
2019-10-09 14:50:04 +0800

    优化

--- src/Coldairarrow.Util/Helper/IpHelper.cs
+++ src/Coldairarrow.Util/Helper/IpHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
@@ -19,9 +18,30 @@ namespace Coldairarrow.Util
         /// <returns></returns>
         public static string GetLocalIp()
         {
-            return Dns.GetHostEntry(Dns.GetHostName()).AddressList
-                .FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork)
-                .ToString();
+            UnicastIPAddressInformation mostSuitableIp = null;
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var network in networkInterfaces)
+            {
+                if (network.OperationalStatus != OperationalStatus.Up)
+                    continue;
+                var properties = network.GetIPProperties();
+                if (properties.GatewayAddresses.Count == 0)
+                    continue;
+
+                foreach (var address in properties.UnicastAddresses)
+                {
+                    if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                        continue;
+                    if (IPAddress.IsLoopback(address.Address))
+                        continue;
+                    return address.Address.ToString();
+                }
+            }
+
+            return mostSuitableIp != null
+                ? mostSuitableIp.Address.ToString()
+                : "";
         }
 
         /// <summary>
