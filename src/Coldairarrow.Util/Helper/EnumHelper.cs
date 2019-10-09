5d14c2ff5f85cbe4154a833a5223d9a0b06f0e6d
Coldairarrow <xgc15657519397@outlook.com>
2019-10-09 14:44:39 +0800

    EnumHelper优化

--- src/Coldairarrow.Util/Helper/EnumHelper.cs
+++ src/Coldairarrow.Util/Helper/EnumHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Coldairarrow.Util
 {
@@ -25,6 +26,40 @@ namespace Coldairarrow.Util
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// 多选枚举转为对应文本,逗号隔开
+        /// </summary>
+        /// <param name="values">多个值</param>
+        /// <param name="enumType">枚举类型</param>
+        /// <returns></returns>
+        public static string ToMultipleText(List<int> values, Type enumType)
+        {
+            if (values == null)
+                return string.Empty;
+
+            List<string> textList = new List<string>();
+
+            var allValues = Enum.GetValues(enumType);
+            foreach (var aValue in allValues)
+            {
+                if (values.Contains((int)aValue))
+                    textList.Add(aValue.ToString());
+            }
+
+            return string.Join(",", textList);
+        }
+
+        /// <summary>
+        /// 多选枚举转为对应文本,逗号隔开
+        /// </summary>
+        /// <param name="values">多个值逗号隔开</param>
+        /// <param name="enumType">枚举类型</param>
+        /// <returns></returns>
+        public static string ToMultipleText(string values, Type enumType)
+        {
+            return ToMultipleText(values?.Split(',')?.Select(x => x.ToInt())?.ToList(), enumType);
         }
     }
 }
