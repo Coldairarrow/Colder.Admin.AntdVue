a18b9fe7c54abb6b9d684115cfca222e14ee6bb7
Coldairarrow <862520575@qq.com>
2019-10-18 22:50:41 +0800

    更新

---
+++ src/Coldairarrow.Web/src/utils/helper/TreeHelper.js
@@ -0,0 +1,30 @@
+//固定列表结构：[{Id:'xxx',ParentId:'xxx'}]
+let TreeHelper = {
+    getParentIds(id, allNodes) {
+        let parents = []
+        let theNode = this.findTheNode(id, allNodes)
+        if (theNode.ParentId) {
+            parents.push(theNode.ParentId)
+            parents.push(...this.getParentIds(theNode.ParentId, allNodes))
+        }
+
+        return parents
+    },
+    getChildrenIds(id, allNodes) {
+        var childrenIds = []
+        let children = allNodes.filter(item => item.ParentId == id)
+            .map(item => item.Id)
+        if (children.length > 0) {
+            childrenIds.push(...children)
+            children.forEach(item => {
+                childrenIds.push(this.getChildrenIds(item, allNodes))
+            })
+        }
+        return childrenIds
+    },
+    findTheNode(id, allNodes) {
+        return allNodes.filter(item => item.Id == id)[0]
+    }
+}
+
+export default TreeHelper
