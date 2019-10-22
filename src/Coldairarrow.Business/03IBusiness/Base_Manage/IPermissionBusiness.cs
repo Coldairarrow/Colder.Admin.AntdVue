5c41b31c8cf721c8218597fe1e2a987bde1f2b59
Coldairarrow <862520575@qq.com>
2019-10-22 21:35:05 +0800

    更新

---
+++ src/Coldairarrow.Business/03IBusiness/Base_Manage/IPermissionBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Coldairarrow.Business.Base_Manage
+{
+    public interface IPermissionBusiness
+    {
+        List<string> GetUserPermissionValues(string userId);
+        List<Base_ActionDTO> GetUserMenuList(string userId);
+    }
+}
