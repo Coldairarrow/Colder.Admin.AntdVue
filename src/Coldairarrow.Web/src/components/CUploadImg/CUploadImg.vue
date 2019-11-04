74634f5a57b67cea1fa425a5947599c2c2346943
Coldairarrow <862520575@qq.com>
2019-11-04 17:02:38 +0800

    图片上传组件

---
+++ src/Coldairarrow.Web/src/components/CUploadImg/CUploadImg.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="clearfix">
+    <a-upload
+      :action="`${$rootUrl}/Base_Manage/Upload/UploadFileByForm`"
+      listType="picture-card"
+      :fileList="fileList"
+      @preview="handlePreview"
+      @change="handleChange"
+      accept="image/*"
+    >
+      <div v-if="fileList.length < maxCount">
+        <a-icon type="plus" />
+        <div class="ant-upload-text">选择</div>
+      </div>
+    </a-upload>
+    <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+      <img alt="example" style="width: 100%" :src="previewImage" />
+    </a-modal>
+  </div>
+</template>
+<script>
+import TypeHelper from '@/utils/helper/TypeHelper'
+const uuid = require('uuid')
+
+export default {
+  props: {
+    value: '', //字符串或字符串数组
+    maxCount: {
+      type: Number,
+      default: 1
+    }
+  },
+  mounted() {
+    if (this.maxCount == 1) {
+      this.value = this.value || ''
+    } else {
+      this.value = this.value || []
+    }
+    this.checkType(this.value)
+
+    this.refresh()
+  },
+  data() {
+    return {
+      previewVisible: false,
+      previewImage: '',
+      fileList: [],
+      obj: {}
+    }
+  },
+  watch: {
+    value(val) {
+      this.checkType(val)
+
+      this.value = val
+      this.refresh()
+    }
+  },
+  methods: {
+    checkType(val) {
+      if (this.maxCount == 1 && TypeHelper.isArray(val)) {
+        throw 'maxCount=1时model不能为Array'
+      }
+      if (this.maxCount > 1 && !TypeHelper.isArray(val)) {
+        throw 'maxCount>1时model必须为Array<String>'
+      }
+    },
+    refresh() {
+      if (this.maxCount < 1) {
+        throw 'maxCount必须>=1'
+      }
+      if (this.value) {
+        let urls = []
+
+        if (TypeHelper.isString(this.value)) {
+          urls.push(this.value)
+        } else if (TypeHelper.isArray(this.value)) {
+          urls.push(...this.value)
+        } else {
+          throw 'value必须为字符串或数组'
+        }
+
+        this.fileList = urls.map(x => {
+          return { name: x, uid: uuid.v4(), status: 'done', url: x }
+        })
+      }
+    },
+    handleCancel() {
+      this.previewVisible = false
+    },
+    handlePreview(file) {
+      this.previewImage = file.url || file.thumbUrl
+      this.previewVisible = true
+    },
+    handleChange({ fileList }) {
+      this.fileList = fileList
+      var urls = this.fileList.filter(x => x.status == 'done').map(x => x.url || x.response.url)
+      var newValue = this.maxCount == 1 ? urls[0] : urls
+      //双向绑定
+      this.$emit('input', newValue)
+    }
+  }
+}
+</script>
+<style>
+/* you can make up upload button and sample style by using stylesheets */
+.ant-upload-select-picture-card i {
+  font-size: 32px;
+  color: #999;
+}
+
+.ant-upload-select-picture-card .ant-upload-text {
+  margin-top: 8px;
+  color: #666;
+}
+</style>
