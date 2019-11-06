9ee6b9d44bf8edf21bc6370846dd6f321affdcae
Coldairarrow <xgc15657519397@outlook.com>
2019-11-06 22:47:54 +0800

    下拉组件

--- src/Coldairarrow.Web/src/components/CSelect/CSelect.vue
+++ src/Coldairarrow.Web/src/components/CSelect/CSelect.vue
@@ -1,37 +1,50 @@
 <template>
+  <!-- <a-form :form="form"> -->
+  <!-- <a-form-item label="" :labelCol="labelCol" :wrapperCol="wrapperCol"> -->
   <a-select
-    allowClear
-    showSearch
-    :filterOption="false"
+    id="select"
+    ref="select"
+    :allowClear="allowClear"
+    :showSearch="showSearch"
+    :filterOption="filterOption"
     @search="handleSearch"
     @change="handleChange"
-    mode="multiple"
-    v-decorator="[{ rules: [{ required: true,message: '必选' }] }]"
+    :mode="mode"
+    v-decorator="['UserList', { rules: [{ required: true, message: '必选' }] }]"
   >
-    <a-select-option v-for="item in UserOptionList" :key="item.value">{{ item.text }}</a-select-option>
+    <a-select-option v-for="item in thisOptions" :key="item.value">{{ item.text }}</a-select-option>
   </a-select>
+  <!-- </a-form-item>
+  </a-form> -->
 </template>
 
 <script>
+import Select from 'ant-design-vue/es/select'
+
 export default {
+  extends: Select,
   props: {
+    value: null,
+    form: null,
     url: {
       //远程获取选项接口地址,接口返回数据结构:[{value:'',text:''}]
       type: String,
       default: null
     },
     allowClear: {
+      //允许清空
       type: Boolean,
       default: true
     },
-    showSearch: {
-      type: Boolean,
-      default: false
+    searchMode: {
+      //搜索模式,'':关闭搜索,'local':本地搜索,'server':服务端搜索
+      type: String,
+      default: ''
     },
     options: {
       //下拉项配置,若无url则必选,结构:[{value:'',text:''}]
       type: Array,
-      default: []
+      default: () => []
     },
     required: {
       type: Boolean,
@@ -42,47 +55,73 @@ export default {
       default: false
     }
   },
+  mounted() {
+    this.mode = this.multiple ? 'multiple' : 'default'
+    if (this.searchMode) {
+      this.showSearch = true
+      if (this.searchMode == 'local') {
+        this.filterOption = true
+      } else {
+        this.filterOption = false
+      }
+    }
+    if (!this.url && this.options.length > 0) {
+      this.thisOptions = this.options
+    }
+    // this.form.getFieldDecorator('select', { rules: [{ required: true, message: '必选' }] })
+    this.reload()
+    // var initValue = { UserList: ['Admin'] }
+    // this.$nextTick(() => {
+    //   this.form.setFieldsValue(initValue)
+    //   this.reloadUser(null, ['Admin'])
+    // })
+  },
   data() {
     return {
+      // form: this.$form.createForm(this),
+      labelCol: { xs: { span: 24 }, sm: { span: 7 } },
+      wrapperCol: { xs: { span: 24 }, sm: { span: 13 } },
       filterOption: false, //本地搜索,非远程搜索
-      options: []
+      thisOptions: [],
+      mode: '',
+      showSearch: false,
+      isInnerchange: false
     }
   },
-  mounted() {
-    var initValue = { UserList: ['Admin'] }
-    this.$nextTick(() => {
-      this.form.setFieldsValue(initValue)
-      this.reloadUser(null, ['Admin'])
-    })
+  watch: {
+    value(value) {
+      if (!this.isInnerchange) {
+        this.$refs.select.$refs.vcSelect.fireChange(value)
+      }
+      this.isInnerchange = false
+    }
   },
   methods: {
-    reloadUser(q, selected) {
+    reload(q) {
+      if (!this.url) {
+        return
+      }
+      let selected = []
+      if (this.multiple) {
+        selected = this.$refs.select.value
+      }
       this.$http
-        .post('/Base_Manage/Base_User/GetOptionList', {
+        .post(this.url, {
           q: q || '',
           selectedValueJson: JSON.stringify(selected || [])
         })
         .then(resJson => {
           if (resJson.Success) {
-            this.UserOptionList = resJson.Data
+            this.thisOptions = resJson.Data
           }
         })
     },
     handleSearch(value) {
-      var selected = this.form.getFieldsValue()['UserList']
-      this.reloadUser(value, selected)
+      this.reload(value)
     },
     handleChange(value) {
-      //   console.log(value)
-      //   this.value = value
-    },
-    handleSubmit() {
-      this.form.validateFields((errors, values) => {
-        //校验成功
-        if (!errors) {
-          console.log('表单数据:', this.form.getFieldsValue())
-        }
-      })
+      this.isInnerchange = true
+      this.$emit('input', value)
     }
   }
 }
---
+++ src/Coldairarrow.Web/src/views/Develop/SelectSearch1.vue
@@ -0,0 +1,57 @@
+<template>
+  <a-card :bordered="false">
+    <a-form :form="form">
+      <a-form-item label="用户选择" :labelCol="labelCol" :wrapperCol="wrapperCol">
+        <c-select
+          :form="form"
+          v-model="entity.UserList"
+          multiple
+          required
+          url="/Base_Manage/Base_User/GetOptionList"
+          searchMode="server"
+        ></c-select>
+      </a-form-item>
+      <a-form-item label="提交" :labelCol="labelCol" :wrapperCol="wrapperCol">
+        <a-button type="primary" @click="handleSubmit()">提交数据</a-button>
+      </a-form-item>
+    </a-form>
+  </a-card>
+</template>
+
+<script>
+import CSelect from '@/components/CSelect/CSelect'
+
+export default {
+  components: {
+    CSelect
+  },
+  data() {
+    return {
+      form: this.$form.createForm(this),
+      labelCol: { xs: { span: 24 }, sm: { span: 7 } },
+      wrapperCol: { xs: { span: 24 }, sm: { span: 13 } },
+      entity: {}
+    }
+  },
+  mounted() {
+    // console.log(11)
+    // var initValue = { UserList: ['Admin'] }
+    // this.$nextTick(() => {
+    //   this.form.setFieldsValue(initValue)
+    //   this.reloadUser(null, ['Admin'])
+    // })
+  },
+  methods: {
+    handleSubmit() {
+      console.log('当前值:', this.entity)
+      this.form.validateFields((errors, values) => {
+        //校验成功
+        if (!errors) {
+          console.log('校验通过')
+          // console.log('表单数据:', this.form.getFieldsValue())
+        }
+      })
+    }
+  }
+}
+</script>
