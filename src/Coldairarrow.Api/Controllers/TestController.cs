4d91809936aefcd369b11bdd187b3b6ca131cdc9
Coldairarrow <xgc15657519397@outlook.com>
2019-11-30 23:07:30 +0800

    压测

--- src/Coldairarrow.Api/Controllers/TestController.cs
+++ src/Coldairarrow.Api/Controllers/TestController.cs
@@ -5,6 +5,7 @@ using Coldairarrow.Util;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Coldairarrow.Api.Controllers
 {
@@ -39,6 +40,48 @@ namespace Coldairarrow.Api.Controllers
             db.Delete(data);
 
             return Success("");
+        }
+
+        [HttpGet]
+        public IActionResult WriteLogTest_EF()
+        {
+            var db = DbFactory.GetRepository();
+            db.Insert(new Base_Log
+            {
+                Id = IdHelper.GetId(),
+                CreateTime = DateTime.Now
+            });
+
+            return Success();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WriteLogTest_FreeSql()
+        {
+            string key = Guid.NewGuid().ToString();
+
+            Task task = null;
+            task = Task.Run(async () =>
+            {
+                try
+                {
+                    await FreeSqlHelper.FreeSql.Insert<Base_Log>().AppendData(new Base_Log
+                    {
+                        Id = IdHelper.GetId()
+                    }).ExecuteAffrowsAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    CacheHelper.SystemCache.RemoveCache(key);
+                }
+            });
+            CacheHelper.SystemCache.SetCache(key, task);
+
+            return await Task.FromResult(Success());
         }
     }
 }
