b733c5f25525e5c990679be65a43680ae39c2a00
Coldairarrow <862520575@qq.com>
2019-12-01 23:43:10 +0800

    压测 自定义连接池

---
+++ src/Coldairarrow.ApiTest/Controllers/TestController.cs
@@ -0,0 +1,65 @@
+﻿using Coldairarrow.Entity.Base_Manage;
+using Coldairarrow.Util;
+using Microsoft.AspNetCore.Mvc;
+using SafeObjectPool;
+using System;
+using System.Threading.Tasks;
+
+namespace Coldairarrow.Api.Controllers
+{
+    [Route("/[controller]/[action]")]
+    public class TestController : BaseController
+    {
+        static ObjectPool<DefaultDbContext> _pool = new ObjectPool<DefaultDbContext>(1000,
+            () => new DefaultDbContext(),
+            obj =>
+            {
+                if (DateTime.Now.Subtract(obj.LastGetTime).TotalSeconds > 5)
+                {
+                    // 对象超过5秒未活动，进行操作
+                }
+            });
+        //public TestController(DefaultDbContext db)
+        //{
+        //    _db = db;
+        //}
+        DefaultDbContext _db { get; }
+        private Base_Log GetNewLog()
+        {
+            return new Base_Log
+            {
+                Id = Guid.NewGuid().ToString(),
+                CreateTime = DateTime.Now
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return HtmlContent("");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WriteLogTest_EFAsync()
+        {
+            using (var db = await _pool.GetAsync())
+            {
+                db.Value.Base_Logs.Add(GetNewLog());
+                await db.Value.SaveChangesAsync();
+            }
+
+            //_db.Base_Logs.Add(GetNewLog());
+            //await _db.SaveChangesAsync();
+
+            return Success();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WriteLogTest_FreeSql()
+        {
+            await FreeSqlHelper.FreeSql.Insert<Base_Log>().AppendData(GetNewLog()).ExecuteAffrowsAsync();
+
+            return Success();
+        }
+    }
+}
---
+++ src/Coldairarrow.ApiTest/FreeSqlHelper.cs
@@ -0,0 +1,16 @@
+﻿using Coldairarrow.Api;
+using FreeSql;
+
+namespace Coldairarrow.Util
+{
+    public static class FreeSqlHelper
+    {
+        static FreeSqlHelper()
+        {
+            FreeSql = new FreeSqlBuilder()
+                .UseConnectionString(DataType.PostgreSQL, DefaultDbContext.ConString)
+                .Build(); //请务必定义成 Singleton 单例模式
+        }
+        public static readonly IFreeSql FreeSql;
+    }
+}
