49888d8ab0d5f01e3c2c1775e2dc59e634cb25cf
Coldairarrow <862520575@qq.com>
2019-12-03 21:58:49 +0800

    更新

--- src/Coldairarrow.ApiTest/Controllers/TestController.cs
+++ src/Coldairarrow.ApiTest/Controllers/TestController.cs
@@ -1,32 +1,16 @@
 ﻿using Coldairarrow.Entity.Base_Manage;
 using Coldairarrow.Util;
 using Microsoft.AspNetCore.Mvc;
-using SafeObjectPool;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Npgsql;
-using System.Linq;
 
 namespace Coldairarrow.Api.Controllers
 {
     [Route("/[controller]/[action]")]
     public class TestController : BaseController
     {
-        static ObjectPool<DefaultDbContext> _pool = new ObjectPool<DefaultDbContext>(500,
-            () => new DefaultDbContext(),
-            obj =>
-            {
-                if (DateTime.Now.Subtract(obj.LastGetTime).TotalSeconds > 5)
-                {
-                    // 对象超过5秒未活动，进行操作
-                }
-            });
-        //public TestController(DefaultDbContext db)
-        //{
-        //    _db = db;
-        //}
-        DefaultDbContext _db { get; }
         private Base_Log GetNewLog()
         {
             return new Base_Log
@@ -37,104 +21,42 @@ namespace Coldairarrow.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult Test()
+        public async Task<IActionResult> SearchTest_1()
         {
-            return HtmlContent("");
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> WriteLogTest_EFAsync()
-        {
-            using (var db = await _pool.GetAsync())
+            using (DefaultDbContext db = new DefaultDbContext())
             {
-                //db.Value.Database.ExecuteSqlCommandAsync
-                db.Value.Base_Logs.Add(GetNewLog());
-                await db.Value.SaveChangesAsync();
+                var data = await db.Base_Logs.FirstOrDefaultAsync();
             }
 
-            //_db.Base_Logs.Add(GetNewLog());
-            //await _db.SaveChangesAsync();
-
-            return Success();
+            return HtmlContent("1");
         }
 
         [HttpGet]
-        public async Task<IActionResult> WriteLogTest_EFNoPool()
+        public async Task<IActionResult> SearchTest_2()
         {
-            using (var db = new DefaultDbContext())
+            var data = await FreeSqlHelper.FreeSql.Select<Base_Log>().FirstAsync();
+
+            return HtmlContent("1");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WriteTest_1()
+        {
+            using (DefaultDbContext db = new DefaultDbContext())
             {
-                //db.Value.Database.ExecuteSqlCommandAsync
-                db.Base_Logs.Add(GetNewLog());
+                await db.Base_Logs.AddAsync(GetNewLog());
                 await db.SaveChangesAsync();
             }
 
-            //_db.Base_Logs.Add(GetNewLog());
-            //await _db.SaveChangesAsync();
-
-            return Success();
+            return HtmlContent("1");
         }
 
         [HttpGet]
-        public IActionResult WriteLogTest_EFSync()
-        {
-            using (var db = _pool.Get())
-            {
-                //db.Value.Database.ExecuteSqlCommandAsync
-                db.Value.Base_Logs.Add(GetNewLog());
-                db.Value.SaveChanges();
-            }
-
-            //_db.Base_Logs.Add(GetNewLog());
-            //await _db.SaveChangesAsync();
-
-            return Success();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> WriteLogTest_FreeSql()
+        public async Task<IActionResult> WriteTest_2()
         {
             await FreeSqlHelper.FreeSql.Insert<Base_Log>().AppendData(GetNewLog()).ExecuteAffrowsAsync();
 
-            return Success();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> StockTest()
-        {
-            string prizeId = "1192732318343761920";
-            string sql = "UPDATE \"Bus_PrizePool\" SET \"UsedStock\"=\"UsedStock\"+1 WHERE \"Id\"=@prizeId AND \"Stock\"-\"UsedStock\">1";
-
-            //var db = new DefaultDbContext();
-            //db.Database.ExecuteSqlCommandAsync(sql, new NpgsqlParameter("@prizeId", prizeId)).ContinueWith(next =>
-            //{
-            //    if (next.Exception != null)
-            //        Console.WriteLine(next.Exception.Message);
-            //    db.Dispose();
-            //    Console.WriteLine("释放成功");
-            //});
-
-            using (var db = await _pool.GetAsync())
-            {
-                await db.Value.Database.ExecuteSqlCommandAsync(sql, new NpgsqlParameter("@prizeId", prizeId));
-            }
-
-            return Success();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> SearchStockTestAsync()
-        {
-            //string prizeId = "1192732318343761920";
-
-            //var db = new DefaultDbContext();
-            //var data = await db.Bus_PrizePools.Where(x => x.Id == prizeId).FirstOrDefaultAsync();
-
-            string id = "global";
-
-            var db = new DefaultDbContext();
-            var data = await db.Bus_Configs.Where(x => x.Id == id).FirstOrDefaultAsync();
-
-            return Success();
+            return HtmlContent("1");
         }
     }
 }
