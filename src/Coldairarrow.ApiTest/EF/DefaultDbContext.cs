86da98ba7a50189f4296b5dede441c372a0a4bea
Coldairarrow <862520575@qq.com>
2019-12-03 11:11:05 +0800

    压测

--- src/Coldairarrow.ApiTest/EF/DefaultDbContext.cs
+++ src/Coldairarrow.ApiTest/EF/DefaultDbContext.cs
@@ -1,17 +1,20 @@
 ﻿using Coldairarrow.Entity.Base_Manage;
+using Coldairarrow.Entity.BusManage;
 using Microsoft.EntityFrameworkCore;
 
 namespace Coldairarrow.Api
 {
     public class DefaultDbContext : DbContext
     {
-        public const string ConString = "SERVER=localhost;PORT=5432;DATABASE=Colder.Admin.AntdVue;USER ID=postgres;PASSWORD=postgres;Pooling=true;MinPoolSize=40;MaxPoolSize=40;";
-        //public const string ConString = "SERVER=61.153.17.10;PORT=5432;DATABASE=db_gxprize_201912;USER ID=u_gxprize_201912;PASSWORD=Cm1nLqOt4t3q-9r4VVWiDdWe;Pooling=true";
+        //public const string ConString = "SERVER=localhost;PORT=5432;DATABASE=Colder.Admin.AntdVue;USER ID=postgres;PASSWORD=postgres;Pooling=true;MinPoolSize=40;MaxPoolSize=40;";
+        public const string ConString = "SERVER=61.153.17.10;PORT=5432;DATABASE=db_gxprize_201912;USER ID=u_gxprize_201912;PASSWORD=Cm1nLqOt4t3q-9r4VVWiDdWe;Pooling=true;MaxPoolSize=40;";
         public DefaultDbContext()
         {
             ChangeTracker.AutoDetectChangesEnabled = false;
         }
         public DbSet<Base_Log> Base_Logs { get; set; }
+        public DbSet<Bus_PrizePool> Bus_PrizePools { get; set; }
+        public DbSet<Bus_Config> Bus_Configs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
