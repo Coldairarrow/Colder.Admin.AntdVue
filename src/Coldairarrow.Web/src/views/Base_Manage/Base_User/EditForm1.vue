72db71139ab1685c3b0b5dd5c2b6d16c38a991bb
Coldairarrow <862520575@qq.com>
2020-04-18 10:04:30 +0800

    前端

---
+++ src/Coldairarrow.Web/src/views/Base_Manage/Base_User/EditForm1.vue
@@ -0,0 +1,52 @@
+<template>
+  <a-form-model ref="ruleForm" :model="ruleForm" :rules="rules" v-bind="layout">
+    <a-form-model-item has-feedback label="Password" prop="pass">
+      <a-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+    </a-form-model-item>
+    <a-form-model-item has-feedback label="Confirm" prop="checkPass">
+      <a-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+    </a-form-model-item>
+    <a-form-model-item has-feedback label="Age" prop="age">
+      <a-input v-model.number="ruleForm.age" />
+    </a-form-model-item>
+    <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="submitForm('ruleForm')">Submit</a-button>
+      <a-button style="margin-left: 10px" @click="resetForm('ruleForm')">Reset</a-button>
+    </a-form-model-item>
+  </a-form-model>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        pass: '',
+        checkPass: '',
+        age: ''
+      },
+      rules: {
+        pass: [{ required: true }]
+      },
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 }
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
