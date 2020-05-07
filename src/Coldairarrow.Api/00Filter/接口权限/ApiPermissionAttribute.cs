039f5acbecda867360ab2c8b8e635ebc36f3ecb5
Coldairarrow <862520575@qq.com>
2020-05-07 21:12:07 +0800

    ApiPermissionAttribute允许多个

--- src/Coldairarrow.Api/00Filter/接口权限/ApiPermissionAttribute.cs
+++ src/Coldairarrow.Api/00Filter/接口权限/ApiPermissionAttribute.cs
@@ -1,5 +1,4 @@
-﻿using Coldairarrow.Business;
-using Coldairarrow.Business.Base_Manage;
+﻿using Coldairarrow.Business.Base_Manage;
 using Coldairarrow.Util;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +10,7 @@ namespace Coldairarrow.Api
     /// <summary>
     /// 接口权限校验
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class ApiPermissionAttribute : BaseActionFilterAsync
     {
         public ApiPermissionAttribute(string permissionValue)
