574c04946cc938553a5b66fdef467342e50abd86
Coldairarrow <862520575@qq.com>
2020-07-05 22:14:25 +0800

    SqlServer脚本乱码修复

--- docs/初始化文件/SQLServer.sql
+++ docs/初始化文件/SQLServer.sql
@@ -19,9 +19,9 @@ Date: 2020-04-19 15:03:39
 -- Table structure for Base_Action
 -- ----------------------------
 CREATE TABLE [Base_Action] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
 [ParentId] nvarchar(50) NULL ,
 [Type] int NOT NULL DEFAULT ((0)) ,
@@ -231,13 +231,13 @@ GO
 -- Table structure for Base_AppSecret
 -- ----------------------------
 CREATE TABLE [Base_AppSecret] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[AppId] varchar(50) NULL ,
-[AppSecret] varchar(50) NULL ,
-[AppName] varchar(50) NULL 
+[AppId] nvarchar(50) NULL ,
+[AppSecret] nvarchar(50) NULL ,
+[AppName] nvarchar(50) NULL 
 )
 
 
@@ -368,15 +368,15 @@ GO
 -- Table structure for Base_BuildTest
 -- ----------------------------
 CREATE TABLE [Base_BuildTest] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[Column1] varchar(50) NULL ,
-[Column2] varchar(50) NULL ,
-[Column3] varchar(50) NULL ,
-[Column4] varchar(50) NULL ,
-[Column5] varchar(50) NULL 
+[Column1] nvarchar(50) NULL ,
+[Column2] nvarchar(50) NULL ,
+[Column3] nvarchar(50) NULL ,
+[Column4] nvarchar(50) NULL ,
+[Column5] nvarchar(50) NULL 
 )
 
 
@@ -535,13 +535,13 @@ GO
 -- Table structure for Base_DbLink
 -- ----------------------------
 CREATE TABLE [Base_DbLink] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[LinkName] varchar(50) NULL ,
-[ConnectionStr] varchar(500) NULL ,
-[DbType] varchar(50) NULL 
+[LinkName] nvarchar(50) NULL ,
+[ConnectionStr] nvarchar(500) NULL ,
+[DbType] nvarchar(50) NULL 
 )
 
 
@@ -672,12 +672,12 @@ GO
 -- Table structure for Base_Department
 -- ----------------------------
 CREATE TABLE [Base_Department] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[Name] varchar(50) NULL ,
-[ParentId] varchar(50) NULL 
+[Name] nvarchar(50) NULL ,
+[ParentId] nvarchar(50) NULL 
 )
 
 
@@ -794,9 +794,9 @@ GO
 -- Table structure for Base_Role
 -- ----------------------------
 CREATE TABLE [Base_Role] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
 [RoleName] nvarchar(50) NULL 
 )
@@ -901,12 +901,12 @@ GO
 -- Table structure for Base_RoleAction
 -- ----------------------------
 CREATE TABLE [Base_RoleAction] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[RoleId] varchar(50) NULL ,
-[ActionId] varchar(50) NULL 
+[RoleId] nvarchar(50) NULL ,
+[ActionId] nvarchar(50) NULL 
 )
 
 
@@ -1023,16 +1023,16 @@ GO
 -- Table structure for Base_User
 -- ----------------------------
 CREATE TABLE [Base_User] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[UserName] varchar(50) NULL ,
-[Password] varchar(50) NULL ,
+[UserName] nvarchar(50) NULL ,
+[Password] nvarchar(50) NULL ,
 [RealName] nvarchar(50) NULL ,
 [Sex] int NOT NULL DEFAULT ((0)) ,
 [Birthday] date NULL ,
-[DepartmentId] varchar(50) NULL 
+[DepartmentId] nvarchar(50) NULL 
 )
 
 
@@ -1205,12 +1205,12 @@ GO
 -- Table structure for Base_UserLog
 -- ----------------------------
 CREATE TABLE [Base_UserLog] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [CreatorRealName] nvarchar(50) NULL ,
-[LogType] varchar(50) NULL ,
-[LogContent] varchar(MAX) NULL 
+[LogType] nvarchar(50) NULL ,
+[LogContent] nvarchar(MAX) NULL 
 )
 
 
@@ -1324,12 +1324,12 @@ GO
 -- Table structure for Base_UserRole
 -- ----------------------------
 CREATE TABLE [Base_UserRole] (
-[Id] varchar(50) NOT NULL ,
+[Id] nvarchar(50) NOT NULL ,
 [CreateTime] datetime NOT NULL ,
-[CreatorId] varchar(50) NULL ,
+[CreatorId] nvarchar(50) NULL ,
 [Deleted] bit NOT NULL DEFAULT ('false') ,
-[UserId] varchar(50) NULL ,
-[RoleId] varchar(50) NULL 
+[UserId] nvarchar(50) NULL ,
+[RoleId] nvarchar(50) NULL 
 )
 
 
