0caaab7b6e1a9532a38cdfe5572bf7d4675e3df6
Coldairarrow <862520575@qq.com>
2020-11-20 22:59:47 +0800

    服务参数

--- src/Coldairarrow.Console/ServiceInstall/install.bat
+++ src/Coldairarrow.Console/ServiceInstall/install.bat
@@ -2,4 +2,6 @@ cd /d %~dp0
 set serviceName="AService"
 set exePath="SocketPassthrough.exe"
 nssm install %serviceName% %~dp0\%exePath%
+::参数设置
+::nssm set emqx AppParameters start
 nssm start %serviceName%
